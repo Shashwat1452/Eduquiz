af919f111cf352839ad24c3c3ebd6fe19bd546ec
Shashwat1452 <shashwat12344321@gmail.com>
2026-06-25 00:20:44 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,101 @@
+//// ================= HOME BUTTON =================
+const startBtn = document.getElementById("startBtn");
+
+if (startBtn) {
+startBtn.onclick = () => {
+location.href = "subjects.html";
+};
+}
+
+// ================= QUIZ DATA =================
+const questions = [
+{
+question: "Which of the following is a rational number?",
+options: ["√2", "π", "3/5", "√5"],
+answer: "3/5"
+},
+{
+question: "The decimal of 7/8 is?",
+options: ["0.875", "0.78", "0.857", "0.708"],
+answer: "0.875"
+},
+{
+question: "Which is irrational?",
+options: ["5", "22/7", "√3", "9"],
+answer: "√3"
+}
+];
+
+let current = 0;
+let score = 0;
+let selected = "";
+
+// ================= ELEMENTS =================
+const q = document.getElementById("question");
+const op1 = document.getElementById("op1");
+const op2 = document.getElementById("op2");
+const op3 = document.getElementById("op3");
+const op4 = document.getElementById("op4");
+const nextBtn = document.getElementById("nextBtn");
+
+// ================= LOAD QUESTION =================
+function loadQuestion() {
+if (!q) return;
+
+q.innerText = questions[current].question;
+
+op1.innerText = questions[current].options[0];
+op2.innerText = questions[current].options[1];
+op3.innerText = questions[current].options[2];
+op4.innerText = questions[current].options[3];
+
+selected = "";
+}
+
+if (q) loadQuestion();
+
+// ================= OPTIONS CLICK =================
+if (op1) op1.onclick = () => selected = op1.innerText;
+if (op2) op2.onclick = () => selected = op2.innerText;
+if (op3) op3.onclick = () => selected = op3.innerText;
+if (op4) op4.onclick = () => selected = op4.innerText;
+
+// ================= NEXT BUTTON =================
+if (nextBtn) {
+nextBtn.onclick = () => {
+
+if (selected === "") {
+alert("Select an option!");
+return;
+}
+
+if (selected === questions[current].answer) {
+score++;
+}
+
+current++;
+
+if (current < questions.length) {
+loadQuestion();
+} else {
+location.href = `result.html?score=${score}&total=${questions.length}`;
+}
+
+};
+}
+
+// ================= RESULT PAGE =================
+const resultBox = document.getElementById("resultBox");
+
+if (resultBox) {
+
+const urlParams = new URLSearchParams(window.location.search);
+
+const score = urlParams.get("score");
+const total = urlParams.get("total");
+
+const percent = Math.round((score / total) * 100);
+
+resultBox.innerHTML =
+`Score: ${score}/${total}<br>Percentage: ${percent}%`;
+}￼Enter
